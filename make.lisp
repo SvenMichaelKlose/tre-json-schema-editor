42c3b662b160d7f1caf988ca7fb2647ff403d296
Sven Michael Klose <pixel@hugbox.org>
2023-11-10 22:17:09 +0100

    Move away docker-related files into docker/.
    They are just a distraction and we do not want anybody to have to
    seek the vital source files of the project.

--- make.lisp
+++ make.lisp
@@ -1,10 +1,15 @@
 (= *modules-path* "/home/pixel/Desktop/git/tre/tre/modules/")
+
 (load (+ *modules-path* "/js/make-js-project.lisp"))
+
 (make-js-project
-  :title    "JSON Schema editor"
-  :outfile  "www/index.html"
+  :title
+    "JSON Schema editor"
+  :outfile
+    "docker/www/index.html"
   :internal-stylesheet
-    (fetch-file "src/style.css")
+    (fetch-file "style.css")
   :files
-    '("src/main.lisp"))
+    '("main.lisp"))
+
 (quit)
