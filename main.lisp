23c3518e10a0254734ae487c1bc3d93c2bba33b0
Sven Michael Klose <pixel@hugbox.org>
2023-12-14 20:36:23 +0100

    Use keyword package for JSON object keys.

--- main.lisp
+++ main.lisp
@@ -1,16 +1,17 @@
 (const *json-schema-basic-types* '("string" "object" "number" "array" "boolean"))
+
 (defclass  (json-schema-title lml-component) (init-attrs)
   (super init-attrs)
-  (set-state (merge {"is_editing" nil} init-attrs))
+  (set-state (merge {:is_editing nil} init-attrs))
   this)
 
 (defmethod  json-schema-title render ()
   (!= props.schema.title
     ($$ (? state.is_editing
-           `(input :grab-focus  t
-                   :on-blur     ,[set-state {is_editing nil}]
+           `(input :grab-focus  1
+                   :on-blur     ,[set-state {:is_editing nil}]
                    :value       ,(| ! ""))
-           `(span :on-click ,[set-state {is_editing t}]
+           `(span :on-click ,[set-state {:is_editing t}]
               ,(+ "(" (| ! "no title") ")"))))))
 
 (finalize-class json-schema-title)
@@ -37,7 +38,7 @@
   (!= state.schema
     ($$ `(table
            (tr (th :class "json-schema-typename" :colspan 2 "object " (json-schema-title :schema ,!)))
-           ,@(maphash #'((k v)  ; TODO: Enable @ to process hash tables and objects.
+           ,@(maphash #'((k v)
                           `(tr
                              (td ,(+ k ":"))
                              (td (json-schema :schema ,v))))
@@ -55,14 +56,14 @@
 
 (fn expand-type (props)
   (? (string? props)
-     {"type" props}
+     {:type props}
      props))
 
 (fn json-schema (attrs)
   (!= (expand-type attrs.schema)
     ($$ `(,(? (string== "object" !.type)
-             'json-schema-object
-             'json-schema-type)
+              'json-schema-object
+              'json-schema-type)
           :schema ,!))))
 
 (declare-lml-component json-schema)
