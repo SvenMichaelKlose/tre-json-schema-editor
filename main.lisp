452b2d1c969f4ea38cf5ca4cac79d615a291d89a
Sven Michael Klose <pixel@hugbox.org>
2023-12-15 01:25:52 +0100

    Can update JSON Schema tree and write it back in one piece.

--- main.lisp
+++ main.lisp
@@ -1,26 +1,33 @@
 (const *json-schema-basic-types* '("string" "object" "number" "array" "boolean"))
 
+
+(fn expand-type (props)
+  (? (string? props)
+     {:type props}
+     props))
+
+
 (defclass  (json-schema-title lml-component) (init-attrs)
   (super init-attrs)
   (set-state (merge {:is_editing nil} init-attrs))
   this)
 
 (defmethod  json-schema-title render ()
-  (!= props.schema.title
+  (!= state.schema.title
     ($$ (? state.is_editing
            `(input :grab-focus  1
                    :on-blur     ,[set-state {:is_editing nil}]
-                   :on-change   ,[state.parent.update :title (_.target.value)]
+                   :on-change   ,[state.parent.update "title" _.target.value]
                    :value       ,(| ! ""))
            `(span :on-click ,[set-state {:is_editing t}]
-              ,(+ "(" (| ! "no title") ")"))))))
+              ,(+ " (" (| ! "no title") ")"))))))
 
 (finalize-class json-schema-title)
 (declare-lml-component json-schema-title)
 
 (fn json-schema-type (attrs)
   (!= attrs.schema
-    ($$ `(div ,!.type (json-schema-title :schema ,!)))))
+    ($$ `(div ,!.type (json-schema-title :schema ,! :parent ,attrs.parent)))))
 
 (declare-lml-component json-schema-type)
 
@@ -31,11 +38,9 @@
   this)
 
 (defmethod json-schema-object update (key val)
-  (!= state
-    (let k !.key    ; TODO: Add key expressions for JSON in tré. (pixel)
-      (replace-state (merge {k val} state))
-      (when !.key
-        (!.parent.update !.key !.schema)))))
+  (= (ref state.schema.properties key) val)
+  (set-state (new))
+  (state.parent.update key state.schema))
 
 (defmethod json-schema-object add (e)
   (!= ($? "<.json-add" e.target)
@@ -45,7 +50,9 @@
 (defmethod json-schema-object render ()
   (!= state.schema
     ($$ `(table
-           (tr (th :class "json-schema-typename" :colspan 2 "object " (json-schema-title :schema ,!)))
+           (tr
+             (th :class "json-schema-typename" :colspan 2
+               "object " (json-schema-title :schema ,! :parent ,this)))
            ,@(maphash #'((k v)
                           `(tr
                              (td ,(+ k ":"))
@@ -62,19 +69,29 @@
 (declare-lml-component json-schema-object)
 
 
-(fn expand-type (props)
-  (? (string? props)
-     {:type props}
-     props))
-
 (fn json-schema (attrs)
   (!= (expand-type attrs.schema)
     ($$ `(,(? (string== "object" !.type)
               'json-schema-object
               'json-schema-type)
-          :schema ,! :key ,attrs.key))))
+          :schema ,! :key ,attrs.key :parent ,attrs.parent))))
 
 (declare-lml-component json-schema)
+
+
+(defclass (json-schema-container lml-component) (init-attrs)
+  (super init-attrs)
+  this)
+
+(defmethod json-schema-container update (dummy-key value)
+  (set-state (new))
+  (props.writer value))
+
+(defmethod json-schema-container render ()
+  ($$ `(json-schema :schema ,props.schema :parent ,this :key nil)))
+
+(finalize-class json-schema-container)
+(declare-lml-component json-schema-container)
 
 
 (var *schema* {
@@ -91,7 +108,10 @@
     }
 })
 
-(document.body.add ($$ `(json-schema :schema ,*schema*)))
+(fn writer (x)
+  (dump x "Updated JSON"))
+
+(document.body.add ($$ `(json-schema-container :schema ,*schema* :writer ,writer)))
 
 (!= (new *mutation-observer
          #'((l o)
